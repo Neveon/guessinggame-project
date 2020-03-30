c81ca48aaa74b86e20710c59bcc9622247229f9c
Neveon <41444780+Neveon@users.noreply.github.com>
2020-03-30 18:31:40 -0400

    Removed empty lines

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
-
 echo "How many files are in the current directory?"
-
+echo ""
 function loop {
-
 	read local input
 	local num_of_files=$(ls | wc -l)
-
 	while [[ ! $input -eq $num_of_files ]]
 	do
-		[[ $input -lt $num_of_files ]] && echo "Guess is too low. Guess again" && read input
-		if [[ $input -gt $num_of_files ]] 
+		if [[ $input -lt $num_of_files ]]
+		then
+			echo "Guess is too low. Guess again" && read input
+		elif [[ $input -gt $num_of_files ]] 
 		then
 			 echo "Guess is too high. Guess again" && read input
 		fi
 	done
-	
 	echo "Congrats! You guessed right!"
 }
-
 loop
